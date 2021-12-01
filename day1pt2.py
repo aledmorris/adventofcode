c7c0532bc1a57144ba5f84069694494a4419996d
aledmorris <aled.morris@pexip.com>
2021-12-01 20:54:07 +1100

    Part 2 first stab

--- day1pt2.py
+++ day1pt2.py
@@ -1,1 +1,23 @@
 # advent of code - day1, part2
+
+# read txt file of the values
+data_file = open('input.txt', 'r')
+Lines = data_file.readlines()
+
+values = []
+increases = 0
+
+# Strips the newline character
+for line in Lines:
+    values.append(int(line))
+
+# process values to count number of increases in the sliding window
+for index,value in enumerate(values):
+    if(index<(len(values)-3)):
+        first = value + values[index+1] + values[index+2]
+        second = values[index+1] + values[index+2] + values[index+3]
+
+        if second > first:
+            increases +=1
+
+print('\nThe number of times the sum of measurements in this sliding window increases: '+str(increases))
