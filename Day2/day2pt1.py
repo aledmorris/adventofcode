cfe994c33d5062fa151776ccf43a6005cf8dc3aa
aledmorris <aled.morris@pexip.com>
2021-12-02 21:00:49 +1100

    Working solution

--- Day2/day2pt1.py
+++ Day2/day2pt1.py
@@ -3,11 +3,6 @@
 x_axis = 0
 y_axis = 0
 
-'''
-forward adds to your horizontal position
-down 5 adds 5 to your depth, resulting in a value of 5.
-up 3 decreases your depth by 3, resulting in a value of 2.
-'''
 actions = []
 values = []
 
@@ -17,7 +12,7 @@ Lines = data_file.readlines()
 
 for line in Lines:
     currentline = line.split(' ')
-    #print(currentline[0]+' -> '+currentline[1])
+    
     if currentline[0].startswith('up'):
         y_axis -=int(currentline[1])
     if currentline[0].startswith('down'):
